14b652076f79d23aca5be865864360a7ed497fad
KarolisGaiv <kgaivenis@gmail.com>
2021-05-05 21:01:24 +0300

    Add condition to prevent playing game with one or 0 players

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Global variables
 let player1;
 let player2;
+let currentPlayer;
 
 const gameBoardModule = (function () {
   const fields = document.querySelectorAll(".board-box");
@@ -8,16 +9,17 @@ const gameBoardModule = (function () {
   const populateBoard = (() => {
     fields.forEach((field) => {
       field.addEventListener("click", () => {
-        player1.setPlayerMarker(field);
+        if (player1 && player2 != null) {
+          currentPlayer.setPlayerMarker(field);
+        } else {
+          alert("You need two players to play this game :)")
+        }
       });
     });
   })();
 
-  return {};
+  return { };
 })();
-
-// const gameController = (function () {
-// })();
 
 const Player = (name, side) => {
   const getName = () => name;
@@ -39,26 +41,28 @@ const submitForm = () => {
   const playerName = document.getElementById("player-name").value;
   const playerMarker = document.querySelector(".player-marker").value;
   const form = document.querySelector(".player-form");
-  const submitBtn = document.querySelector(".submit-btn")
+  const submitBtn = document.querySelector(".submit-btn");
 
-  // Check if player is created first time and remove side option after it was taken
-  if(player1 == null) {
-    player1 = Player(playerName, playerMarker)
+  // Check if player is created first time and remove marker option after it was taken
+  if (player1 == null) {
+    player1 = Player(playerName, playerMarker);
     document.getElementById(playerMarker).remove();
+    getCurrentPlayer(player1);
   } else {
-    player2 = Player(playerName, playerMarker)
+    player2 = Player(playerName, playerMarker);
+    currentPlayer = player2;
   }
 
   // Disable player creation if two players already exist
-  if(player1 && player2 != null) {
-    submitBtn.disabled = true
+  if (player1 && player2 != null) {
+    submitBtn.disabled = true;
   }
 
   form.reset();
 };
 
-
-
-
-
-
+const getCurrentPlayer = (player) => {
+  if (player.playerSide == "X") {
+    currentPlayer = player;
+  }
+};
