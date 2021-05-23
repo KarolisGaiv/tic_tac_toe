c33ff57eef7df10f6532a630abd206c6c1491dda
KarolisGaiv <kgaivenis@gmail.com>
2021-05-23 13:12:39 +0300

    Edit variables and fix typo mistakes

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 let player1;
 let player2;
 let currentPlayer;
+const darkSide = "X";
 
 const gameBoardModule = (function () {
-  const fields = document.querySelectorAll(".board-box");
   let counter = 0;
-  let gameWon = false;
+  let isGameEnded = false;
 
   const winningPositions = [
     [0, 1, 2],
@@ -19,39 +19,47 @@ const gameBoardModule = (function () {
     [2, 4, 6],
   ];
 
+  const fields = document.querySelectorAll(".board-box");
   const populateBoard = (() => {
     fields.forEach((field) => {
       field.addEventListener("click", () => {
-        // Check if both players exist before setting player marker
-        if (!player1 || !player2) {
-          alert("You need two players to play this game :)");
-        }
-        // Check if square is not taken
-        if (isPopulated(field)) {
-          alert("Field is already taken, choose another one");
-          return;
-        }
-        currentPlayer.setPlayerMarker(field);
-        counter++;
-        // Start checking for winner only when one player has already made at least 2 moves
-        if (counter > 4) {
-          checkResult(counter, currentPlayer);
-        }
-        if (!gameWon) {
-          changePlayerTurn();
-        }
+        boxClicked(field);
       });
     });
   })();
 
+  function boxClicked(field) {
+    // Check if both players exist before setting player marker
+    if (!player1 || !player2) {
+      alert("You need two players to play this game :)");
+      return;
+    }
+    // Check if square is not taken
+    if (isPopulated(field)) {
+      alert("Field is already taken, choose another one");
+      return;
+    }
+    makeMove(field);
+  }
+
   function isPopulated(element) {
-    let status = false;
-    element.innerHTML ? (status = true) : (status = false);
-    return status;
+    return element.innerHTML;
+  }
+
+  function makeMove(field) {
+    currentPlayer.setPlayerMarker(field);
+    counter++;
+    const minMovesToWin = 4;
+    if (counter > minMovesToWin) {
+      checkResult(counter, currentPlayer);
+    }
+    if (!isGameEnded) {
+      changePlayerTurn();
+    }
   }
 
   const changePlayerTurn = () => {
-    if (currentPlayer == player1) {
+    if (currentPlayer === player1) {
       currentPlayer = player2;
     } else {
       currentPlayer = player1;
@@ -60,38 +68,35 @@ const gameBoardModule = (function () {
 
   const checkResult = (turnCount, currentPlayer) => {
     checkPlayerTurns(currentPlayer.playerArray);
+    const maxTurnCount = 9;
 
-    if (!gameWon) {
-      if (turnCount > 8) {
-        alert("It's a TIE!");
-        return;
-      }
+    if (!isGameEnded && turnCount === maxTurnCount) {
+      alert("It's a TIE!");
+      return;
     }
   };
 
   const checkPlayerTurns = (playerArray) => {
     winningPositions.forEach((winningCombo) => {
-      if (compareArrays(playerArray, winningCombo)) {
-        showWinMessage(winningCombo);
-        gameWon = true;
+      if (compareArrays(winningCombo, playerArray)) {
+        showEndResult(winningCombo);
+        isGameEnded = true;
       }
     });
   };
 
-  const compareArrays = (arr1, arr2) => {
-    let signal = true;
-    arr2.forEach((el) => {
-      if (!arr1.includes(el)) {
-        signal = false;
-      }
-    });
-    return signal;
-  };
+  const compareArrays = (arr1, arr2) =>
+    arr1.every((element) => arr2.includes(element));
+
+  function showEndResult(playerArr) {
+    markWinningSquares(playerArr);
+    showWinMessage();
+  }
 
   const markWinningSquares = (winningArray) => {
     winningArray.forEach((element) => {
-      let squareToHiglight = document.getElementById(element);
-      if (currentPlayer.playerSide == "X") {
+      const squareToHiglight = document.getElementById(element);
+      if (currentPlayer.playerSide === darkSide) {
         squareToHiglight.classList.add("dark-side");
       } else {
         squareToHiglight.classList.add("light-side");
@@ -99,26 +104,22 @@ const gameBoardModule = (function () {
     });
   };
 
-  const showWinMessage = (winningArray) => {
-    markWinningSquares(winningArray);
+  const showWinMessage = () => {
+    //setTimout here helps Chrome to render CSS properly when confirm is triggered.
+    // https://stackoverflow.com/questions/3787105/weird-problem-with-settimeout-on-google-chrome
 
     setTimeout(function () {
-      if (confirm(winMessage())) {
+      if (confirm(getWinMessage())) {
         restartBoard();
-      } else {
-        return false;
       }
     }, 0);
   };
 
-  const winMessage = () => {
-    let message;
-    if (currentPlayer.playerSide == "X") {
-      message = `Congratz - Dark Side with ${currentPlayer.name} Won! Do you want to restart game?`;
-    } else {
-      message = `Congratz - Light Side with ${currentPlayer.name} Won! Do you want to restart game?`;
-    }
-    return message;
+  const getWinMessage = () => {
+    const side =
+      currentPlayer.playerSide === darkSide ? "Dark side" : "Light side";
+
+    return `Congratz - ${side} with ${currentPlayer.name} Won! Do you want to restart the game?`;
   };
 
   const restartBoard = () => {
@@ -133,15 +134,15 @@ const gameBoardModule = (function () {
       field.className = "board-box";
     });
     counter = 0;
-    gameWon = false;
+    isGameEnded = false;
   }
 
   return { clearFields };
 })();
 
 const Player = (name, side) => {
-  let playerSide = side;
-  let playerArray = [];
+  const playerSide = side;
+  const playerArray = [];
   const darkSideIcon = "swg swg-darthvader-5 swg-4x";
   const lightSideIcon = "swg swg-yoda-3 swg-4x";
   const darkSideIconSmall = "swg swg-darthvader-5";
@@ -149,7 +150,7 @@ const Player = (name, side) => {
 
   function setPlayerMarker(square) {
     const icon = document.createElement("span");
-    if (playerSide == "X") {
+    if (playerSide === darkSide) {
       icon.className = darkSideIconSmall;
       icon.style.color = "red";
       square.appendChild(icon);
@@ -158,7 +159,6 @@ const Player = (name, side) => {
       icon.style.color = "whitesmoke";
       square.appendChild(icon);
     }
-    // square.innerText = playerSide;
     saveTurn(square);
   }
 
@@ -171,7 +171,7 @@ const Player = (name, side) => {
     // Create player card
     const playerCard = document.createElement("div");
     playerCard.className = "player-card";
-    side == "X"
+    side === darkSide
       ? playerCard.classList.add("dark-side")
       : playerCard.classList.add("light-side");
     // Place player card inside player card wrapper
@@ -190,17 +190,16 @@ const Player = (name, side) => {
     const cardMarker = document.createElement("div");
     cardMarker.className = "card-marker";
     // Place player side inside card detail
-    if (side == "X") {
+    if (side === darkSide) {
       cardMarker.innerText = "Dark Side";
-      detailsWrapper.appendChild(cardMarker);
     } else {
       cardMarker.innerText = "Light Side";
-      detailsWrapper.appendChild(cardMarker);
     }
+    detailsWrapper.appendChild(cardMarker);
     // Create card icon
     const cardIcon = document.createElement("span");
     cardIcon.id = "player-icon";
-    if (side == "X") {
+    if (side === darkSide) {
       cardIcon.className = darkSideIcon;
       playerCard.appendChild(cardIcon);
     } else {
@@ -216,34 +215,34 @@ const Player = (name, side) => {
 
 const submitForm = () => {
   const playerName = document.getElementById("player-name").value;
-  const playerMarker = document.querySelector(".player-marker").value;
-  const form = document.querySelector(".player-form");
 
   if (!playerName) {
     alert("Please enter player name");
     return;
   }
 
+  const playerMarker = document.querySelector(".player-marker").value;
+  const form = document.querySelector(".player-form");
+
   // Check if player is created first time and remove marker option after it was taken
-  if (player1 == null) {
+  if (!player1) {
     player1 = Player(playerName, playerMarker);
-    document.getElementById(playerMarker).disabled = true;
     playerName.innerHTML = "";
-    getCurrentPlayer(player1);
+    setStartingPlayer(player1);
     player1.createPlayerCard(player1.name, player1.playerSide);
   } else {
     player2 = Player(playerName, playerMarker);
-    document.getElementById(playerMarker).disabled = true;
-    getCurrentPlayer(player2);
+    setStartingPlayer(player2);
     player2.createPlayerCard(player2.name, player2.playerSide);
   }
+  document.getElementById(playerMarker).disabled = true;
   toogleForm();
   form.reset();
 };
 
-const getCurrentPlayer = (player) => {
-  if (player.playerSide == "X") {
-    currentPlayer = player;
+const setStartingPlayer = (player) => {
+  if (player.playerSide === darkSide) {
+    currentPlayer = player1;
   }
 };
 
@@ -267,16 +266,16 @@ function resetFormOptions() {
 }
 
 function deleteCards() {
-  const playerCard = document.querySelectorAll(".player-card");
-  playerCard.forEach((card) => {
+  const playerCards = document.querySelectorAll(".player-card");
+  playerCards.forEach((card) => {
     card.remove();
   });
 }
 
 function reset() {
-  player1 = undefined;
-  player2 = undefined;
-  currentPlayer = undefined;
+  player1 = null;
+  player2 = null;
+  currentPlayer = null;
 
   toogleForm();
   resetFormOptions();
